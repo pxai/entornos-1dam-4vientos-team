f9b93fdac4cee6183bf674a7a814fe0ca7d8a3d5
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2012-11-18 23:26:40 +0000

    primer import

---
+++ trunk/Ejercicio2ArrayMetodos/src/Ejercicio2.java
@@ -0,0 +1,20 @@
+/**
+ * $Id$
+ */
+
+/**
+ * @author pello altadill
+ * @version 0.1
+ */
+public class Ejercicio2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] números = new int[15];
+		
+	}
+
+}
