c8a512cecc438e4890a9aacf9e01602ac4f9e229
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-01-18 08:16:02 +0000

    ok

---
+++ Tema11Ejercicio2/src/Carta.java
@@ -0,0 +1,26 @@
+
+public class Carta {
+private String nombre;
+private String palo;
+private int valor;
+/**
+ * @param nombre
+ * @param palo
+ * @param valor
+ */
+public Carta(String nombre, String palo, int valor) {
+	this.nombre = nombre;
+	this.palo = palo;
+	this.valor = valor;
+}
+/**
+ * devuelve una cadena con la info de la carta
+ * @return String
+ */
+@Override
+public String toString() {
+	return "Carta [nombre=" + nombre + ", palo=" + palo + ", valor=" + valor
+			+ "]";
+}
+
+}
