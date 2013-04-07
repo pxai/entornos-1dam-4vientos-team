0efa5a06a064c525ac86ed03f788718407e215ee
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-04-07 22:42:23 +0000

    ok

---
+++ HolaAntJunit/src/Saludo.java
@@ -0,0 +1,20 @@
+/**
+ * Tremenda clase que es capaz de saludar en más
+ * de un idioma.
+ * @author pello altadill
+ *
+ */
+public class Saludo {
+	public Saludo () {
+		
+	}
+	
+	/**
+	 * devuelve el saludo en castellano
+	 * @return
+	 */
+	public String saludoCastellano () {
+		return "hola";
+	}
+}
+
