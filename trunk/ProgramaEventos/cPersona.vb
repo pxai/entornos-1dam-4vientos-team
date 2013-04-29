56334622d8abba24b971841092f05115d76330ad
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-04-29 09:28:42 +0000

    Primer commit

---
+++ trunk/ProgramaEventos/cPersona.vb
@@ -0,0 +1,43 @@
+﻿Public Class cPersona
+    Private _nombre As String
+    Private _sueldo As Double
+    Public Event LimiteSueldo(ByVal sender As cPersona, ByVal e As System.EventArgs)
+    Public Event SuperSueldo(ByVal sender As cPersona, ByVal e As System.EventArgs)
+
+    Public Property nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(ByVal value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property sueldo As Double
+        Get
+            Return _sueldo
+        End Get
+        Set(ByVal value As Double)
+            If value <= 0 Then
+                Throw New Exception("El sueldo debe ser mayor que 0")
+            ElseIf value < 1000 Then
+                Dim miE As New EventArgs
+                RaiseEvent LimiteSueldo(Me, miE)
+            ElseIf value > 10000 Then
+
+
+            Else
+                _sueldo = value
+            End If
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' constructor vacío
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub cPersona()
+
+    End Sub
+
+End Class
