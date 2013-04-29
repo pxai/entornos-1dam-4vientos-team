26d5c8b1f1a330c88ef5db3ae33e15a10fe1bfdd
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-04-29 09:38:26 +0000

    Añadido EventArg personalizado para SuperSueldo

---
+++ ProgramaEventos/SuperSueldoEventArg.vb
@@ -0,0 +1,22 @@
+﻿Public Class SuperSueldoEventArg : Inherits EventArgs
+    Public Property SueldoAIntroducir As Double
+    Public Property Admitirlo As Boolean
+
+    ''' <summary>
+    ''' constructor vacío
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub New()
+
+    End Sub
+
+    ''' <summary>
+    ''' Constructor con parámetro
+    ''' 
+    ''' </summary>
+    ''' <param name="sueldoAIntroducir"></param>
+    ''' <remarks></remarks>
+    Public Sub New(ByVal sueldoAIntroducir As Double)
+        Me.SueldoAIntroducir = sueldoAIntroducir
+    End Sub
+End Class
