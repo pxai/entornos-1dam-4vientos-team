756de4c96220d68530749335e1a02ca1fc0ae6ec
pruebas@cuatrovientos.org <pruebas@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-04-30 08:04:37 +0000

    Cambios en proyecto

--- trunk/ProyectoComunitario/Form1.vb
+++ trunk/ProyectoComunitario/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         FormPello.Show()
     End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        FormPruebas.Show()
+    End Sub
 End Class
