c3027e9cecc7e25fb94c4ed9b9c7c468397c2fb5
pruebas@cuatrovientos.org <pruebas@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-04-30 08:10:08 +0000

    ok

--- trunk/ProyectoComunitario/FormPruebas.Designer.vb
+++ trunk/ProyectoComunitario/FormPruebas.Designer.vb
@@ -22,8 +22,29 @@ Partial Class FormPruebas
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(89, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Esto es de prueba"
+        '
+        'FormPruebas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "FormPruebas"
         Me.Text = "FormPruebas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
