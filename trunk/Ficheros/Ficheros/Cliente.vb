cad1bc2c1f9cac03ad49c0a95fcd62575ca75192
ehelia_roque@alumnado.cuatrovientos.org <ehelia_roque@alumnado.cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-05-10 11:01:31 +0000

    Primer commit

---
+++ trunk/Ficheros/Ficheros/Cliente.vb
@@ -0,0 +1,17 @@
+﻿Public Class Cliente : Implements IEquatable(Of Cliente)
+    Public Property Servidor As String
+    Public Property Usuario As String
+    Public Property Contraseña As String
+    Public Property Puerto As Integer
+
+    
+    Public Overrides Function ToString() As String
+        Return Usuario & "@" & Servidor
+    End Function
+    Public Overloads Function Equals(ByVal other As Cliente) As Boolean Implements System.IEquatable(Of Cliente).Equals
+        Return Equals(Me, other)
+    End Function
+    Public Overloads Shared Function Equals(ByVal cliente1 As Cliente, ByVal cliente2 As Cliente) As Boolean
+        Return cliente1.Servidor = cliente2.Servidor And cliente1.Usuario = cliente2.Usuario
+    End Function
+End Class
---
+++ trunk/Ficheros/Ficheros/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnListar = New System.Windows.Forms.Button()
+        Me.lstClientes = New System.Windows.Forms.ListBox()
+        Me.btnAñadir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtContraseña = New System.Windows.Forms.TextBox()
+        Me.txtPuerto = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnListar
+        '
+        Me.btnListar.Location = New System.Drawing.Point(344, 50)
+        Me.btnListar.Name = "btnListar"
+        Me.btnListar.Size = New System.Drawing.Size(75, 23)
+        Me.btnListar.TabIndex = 0
+        Me.btnListar.Text = "Listar"
+        Me.btnListar.UseVisualStyleBackColor = True
+        '
+        'lstClientes
+        '
+        Me.lstClientes.FormattingEnabled = True
+        Me.lstClientes.Location = New System.Drawing.Point(305, 107)
+        Me.lstClientes.Name = "lstClientes"
+        Me.lstClientes.Size = New System.Drawing.Size(171, 108)
+        Me.lstClientes.TabIndex = 1
+        '
+        'btnAñadir
+        '
+        Me.btnAñadir.Location = New System.Drawing.Point(52, 251)
+        Me.btnAñadir.Name = "btnAñadir"
+        Me.btnAñadir.Size = New System.Drawing.Size(88, 43)
+        Me.btnAñadir.TabIndex = 2
+        Me.btnAñadir.Text = "Añadir Cliente"
+        Me.btnAñadir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(86, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(86, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Usuario"
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(157, 91)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsuario.TabIndex = 5
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(157, 52)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 6
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(190, 254)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(88, 38)
+        Me.btnBorrar.TabIndex = 7
+        Me.btnBorrar.Text = "Borrar Cliente"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(332, 251)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(101, 43)
+        Me.btnGuardar.TabIndex = 8
+        Me.btnGuardar.Text = "Guardar Clientes"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(86, 132)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Contraseña"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(86, 167)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Puerto"
+        '
+        'txtContraseña
+        '
+        Me.txtContraseña.Location = New System.Drawing.Point(157, 128)
+        Me.txtContraseña.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtContraseña.Name = "txtContraseña"
+        Me.txtContraseña.Size = New System.Drawing.Size(76, 20)
+        Me.txtContraseña.TabIndex = 11
+        '
+        'txtPuerto
+        '
+        Me.txtPuerto.Location = New System.Drawing.Point(157, 162)
+        Me.txtPuerto.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtPuerto.Name = "txtPuerto"
+        Me.txtPuerto.Size = New System.Drawing.Size(76, 20)
+        Me.txtPuerto.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(488, 411)
+        Me.Controls.Add(Me.txtPuerto)
+        Me.Controls.Add(Me.txtContraseña)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnAñadir)
+        Me.Controls.Add(Me.lstClientes)
+        Me.Controls.Add(Me.btnListar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnListar As System.Windows.Forms.Button
+    Friend WithEvents lstClientes As System.Windows.Forms.ListBox
+    Friend WithEvents btnAñadir As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtUsuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents btnBorrar As System.Windows.Forms.Button
+    Friend WithEvents btnGuardar As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtContraseña As System.Windows.Forms.TextBox
+    Friend WithEvents txtPuerto As System.Windows.Forms.TextBox
+
+End Class
