a4692b3c4dc5f9b5232406c5246962494eea7e52
ehelia_roque@alumnado.cuatrovientos.org <ehelia_roque@alumnado.cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-05-10 11:01:31 +0000

    Primer commit

---
+++ Ficheros/Ficheros/Clientes.vb
@@ -0,0 +1,72 @@
+﻿Imports System.IO 'Asi no hace falta poner IO 
+Public Class Clientes
+    Private _lstClientes As New List(Of Cliente)
+    Public ReadOnly Property LstClientes As ObjectModel.ReadOnlyCollection(Of Cliente)
+        Get
+            Return _lstClientes.AsReadOnly
+        End Get
+    End Property
+
+    Public Sub New()
+        Dim fich As IO.StreamReader
+        'Se puede preguntar si exixte con la siguiente instruccion 
+        'If Not IO.File.Exists("Perros.txt") Then Exit Sub
+        Try
+            fich = New IO.StreamReader("Clientes.txt")
+            'fich.EndOfStream ' Si ha llegado a fin de  fichero
+            Do While Not fich.EndOfStream
+                Dim miCliente As New Cliente
+                miCliente.Servidor = fich.ReadLine()
+                miCliente.Usuario = fich.ReadLine()
+                miCliente.Contraseña = fich.ReadLine()
+                miCliente.Puerto = fich.ReadLine()
+                _lstClientes.Add(miCliente)
+            Loop
+            fich.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Public Overloads Function Añadir(ByVal servidor As String, ByVal usuario As String, ByVal contraseña As String, ByVal puerto As Integer) As Cliente
+        Dim cliente As New Cliente
+        cliente.Servidor = servidor
+        cliente.Usuario = usuario
+        cliente.Contraseña = contraseña
+        cliente.Puerto = puerto
+        If _lstClientes.Contains(cliente) Then Return Nothing
+        _lstClientes.Add(cliente)
+        Return cliente
+    End Function
+    Public Overloads Function Añadir(ByVal cliente As Cliente) As Cliente
+        If _lstClientes.Contains(cliente) Then Return Nothing
+        _lstClientes.Add(cliente)
+        'Se puede guardar (solo este CLIENTE) abriendo el fichero con el 2º parametro 
+        'de append a cierto (boolean)
+        'Dim fich As New IO.StreamWriter("Clientes.txt", True)
+        'fich.WriteLine(cliente.Servidor)
+        'fich.WriteLine(cliente.Usuario)
+        'fich.WriteLine(cliente.Contraseña)
+        'fich.WriteLine(cliente.Puerto)
+        'fich.Close()
+        Return cliente
+    End Function
+    Public Function Borrar(ByVal cliente As Cliente) As String
+        'El Contains y el Remove funciona porque sobrecarga el Equals
+        If Not _lstClientes.Contains(cliente) Then
+            Return "Cliente inexistente"
+        End If
+        _lstClientes.Remove(cliente)
+        Return "Cliente borrado"
+    End Function
+    Public Sub GuardarCliente()
+        Dim fich As New IO.StreamWriter("Clientes.txt") 'Crea  el fichero nuevo
+        For Each MiCliente As Cliente In _lstClientes
+            fich.WriteLine(MiCliente.Servidor)
+            fich.WriteLine(MiCliente.Usuario)
+            fich.WriteLine(MiCliente.Contraseña)
+            fich.WriteLine(MiCliente.Puerto)
+        Next
+        fich.Close()
+    End Sub
+
+End Class
---
+++ Ficheros/Ficheros/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Private misClientes As Clientes
+
+    Private Sub btnListar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnListar.Click
+        misClientes = New Clientes
+        lstClientes.DataSource = misClientes.LstClientes 'Si no hay DisplayMember muestra por ToString
+        'lstPerros.DisplayMember = "Nombre" 'Campo a mostrar
+    End Sub
+
+    Private Sub btnAñadir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAñadir.Click
+        Dim miCliente As New Cliente
+
+        miCliente.Servidor = txtNombre.Text
+        miCliente.Usuario = txtUsuario.Text
+        miCliente.Contraseña = txtContraseña.Text
+        If Not IsNumeric(txtPuerto.Text) Then
+            MessageBox.Show("Introduce un número")
+            txtPuerto.Focus()
+        Else
+            miCliente.Puerto = txtPuerto.Text
+            'misClientes = New Clientes
+            misClientes.Añadir(miCliente)
+        End If
+        'Si no funciona solo con DataSource, agregamos la siguiente linea
+        'Para que se disvincule
+        lstClientes.DataSource = Nothing
+        lstClientes.DataSource = misClientes.LstClientes
+        If misClientes.Añadir(miCliente) Is Nothing Then
+            MessageBox.Show("Cliente ya existe")
+
+        End If
+    End Sub
+
+    Private Sub btnBorrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBorrar.Click
+        Dim re As String
+        Dim miCliente As New Cliente
+        miCliente.Servidor = txtNombre.Text
+        miCliente.Usuario = txtUsuario.Text
+        miCliente.Contraseña = txtContraseña.Text
+        miCliente.Puerto = txtPuerto.Text
+        re = misClientes.Borrar(miCliente)
+        lstClientes.Items.Remove(miCliente)
+        If re.Contains("inexistente") Then
+            MessageBox.Show("Cliente no existe")
+        Else
+            MessageBox.Show("Dale a guardar para q se guarden los cambios")
+        End If
+    End Sub
+
+    Private Sub btnGuardar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGuardar.Click
+        misClientes.GuardarCliente()
+    End Sub
+
+End Class
