e20533c4d74b4429559f5546a0a214dbe28d4933
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-05-22 12:16:03 +0000

    ok

---
+++ 00Refactorings/src/org/cuatrovientos/LEGIONES/princeps.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.cuatrovientos.LEGIONES;
+
+/**
+ * @author luser
+ *
+ */
+public class princeps extends unidaz {
+	String nombre;
+	int vida;
+
+	/**
+	 * 
+	 */
+	public princeps() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param at
+	 * @param df
+	 */
+	public princeps(int at, int df) {
+		super(at, df);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ 00Refactorings/src/org/cuatrovientos/LEGIONES/velites.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.cuatrovientos.LEGIONES;
+
+/**
+ * @author luser
+ *
+ */
+public class velites extends unidaz {
+	String nombre;
+	int vida;
+
+	/**
+	 * 
+	 */
+	public velites() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param at
+	 * @param df
+	 */
+	public velites(int at, int df) {
+		super(at, df);
+		// TODO Auto-generated constructor stub
+	}
+
+}
