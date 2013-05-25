529f98e9ca14c4349635e9643597e2b21a16d26e
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-05-25 13:36:06 +0000

    imperial fleet, best regards. "you don't meet a girl like that every dinasty" China's Emperor words in Mulan

---
+++ trunk/00Refactorings/src/org/cuatrovientos/EMPIRE_FLEET/tie_bomber.java
@@ -0,0 +1,12 @@
+package org.cuatrovientos.EMPIRE_FLEET;
+
+/**
+ * Represents a Tie Bomber
+ * @author Sienar Fleet Systems
+ * @greets the emperor
+ */
+public class tie_bomber {
+	int range;
+	int energy;
+	String pilot_name;
+}
---
+++ trunk/00Refactorings/src/org/cuatrovientos/EMPIRE_FLEET/tie_fighter.java
@@ -0,0 +1,12 @@
+package org.cuatrovientos.EMPIRE_FLEET;
+
+/**
+ * Represents a Tie Fighter starfighter
+ * @author Sienar Fleet Systems
+ * @greets the emperor
+ */
+public class tie_fighter {
+	int range;
+	int energy;
+	String pilot_name;
+}
