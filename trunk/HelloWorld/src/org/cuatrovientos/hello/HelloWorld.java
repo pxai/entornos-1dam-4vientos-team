10aa3b557e7acd215d43d95560beccd0c7c7d4e7
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@ddb867e5-02cf-8f5e-fa31-5203c9c82c96>
2013-10-01 11:04:35 +0000

    Subo proyecto google code

---
+++ trunk/HelloWorld/src/org/cuatrovientos/hello/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.cuatrovientos.hello;
+
+/**
+ * A class that says hello world 
+ * @author pello_altadill
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Shows hello world on console
+		System.out.println("Hello World!");
+
+	}
+
+}
